7b0b4887a370c02d1d3262dd79db70e2b98a6a60
Danny Antaki <dantaki@ucsd.edu>
2017-04-18 11:34:02 -0700

    Create job_array.sh

---
+++ job_array.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --partition=shared
+#SBATCH -c 16 # Number of CPUs for each job spawned. Hence, each job will use 16 cores
+#SBATCH -t 0-04:00 # Walltime Days-Hours-Minutes
+#SBATCH --job-name=FOO
+#SBATCH -o PATH_TO_LOGS/FOO_%a_out # %a is the job array index
+#SBATCH -e PATH_TO_LOGS/FOO_%a_err
+#SBATCH --array=1-8%4 # Job array argument. Run 8 jobs, running 4 at the same time: 1-N%NUMBER_SIMULTANEOUS_JOBS
+
+SEED=$(cat FILE_WITH_COMMANDS | head -n $SLURM_ARRAY_TASK_ID | tail -n 1 | awk "{print $1}")
+$SEED
+
+# FILE_WITH_COMMANDS is a file that contains the commands you wish to run.
+# For example, the first line could be 'echo "hello world 1"', the second line will be 'echo "hello world 2"' and so on...
+# When initalized, this example will submit the first four lines as a separate job, when one completes the 5th line is submitted
+#    until the command on the 8th line is finished. 
