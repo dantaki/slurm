fe9136a49efda41426576e00ffb29e6d46c7e6c0
Danny Antaki <dantaki@ucsd.edu>
2017-04-18 11:37:51 -0700

    Update slurm.sh

--- slurm.sh
+++ slurm.sh
@@ -7,6 +7,9 @@
 #SBATCH -o PATH_TO_LOGS/FOO_out # standard output
 #SBATCH -e PATH_TO_LOGS/FOO_err # error messages
 
+##SBATCH --dependency=afterany:SLURM_JOB_ID # This argument sets up a dependency that will execute the commands once SLURM_JOB_ID completes
+
+
 ##SBATCH --mail-type=ALL # Send init, completion, and errors
 ##SBATCH --mail-user=MY_EMAIL@foo.com
 
